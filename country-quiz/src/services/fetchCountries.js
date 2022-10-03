fab72fe841a68c45d2b544d7421ce274900bf8e4
Bilal Özgen <73560456+bozgen@users.noreply.github.com>
2022-10-03 18:41:29 +0300

    Delete country-quiz/src directory

--- country-quiz/src/services/fetchCountries.js
+++
@@ -1,6 +0,0 @@
-const fetchCountries = async () => {
-    const res = await fetch("https://restcountries.com/v3.1/all");
-    const data = await res.json();
-    return data;
-}
-export default fetchCountries;
